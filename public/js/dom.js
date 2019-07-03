3b9fcdae549f94ff9aec47122b4edd3caeb53b21
wajdomahdi <eparis@atlassian.com>
2019-07-03 11:23:02 +0300

    translation API Related #7

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,22 +1,22 @@
-fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190703T060408Z.9a2c739ed70efe69.b5f4e317e79bd07c2d4923694f9c82b15c2aea60&text='+word+'&lang=ar-en')
-  .then(response => {
-    // here we're turning the response into JSON.
-    return response.json();
-  })
-  .then(data => {
-
-      
-      document.getElementById("submit").addEventListener("click", function(){
-       var word= document.getElementById("add").value;
-        document.querySelector("p").innerText= data.text[0];
-
-
-        
-      });
-      
-    // Here's a list of repos!
-    console.log(data);
-  })
+document.getElementById("submit").addEventListener("click", function() {
+  var word = document.getElementById("word").value;
+  fetch(
+    "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190703T060408Z.9a2c739ed70efe69.b5f4e317e79bd07c2d4923694f9c82b15c2aea60&text=" +
+      word +
+      "&lang=ar-en"
+  )
+    .then(response => {
+      // here we're turning the response into JSON.
+      return response.json();
+      console.log(response.json);
+    })
+    .then(data => {
+      console.log(data);
+      document.querySelector("p").innerText = data.text[0];
+    })
     .catch(error => {
-    console.log(error);
-  });
+      console.log(error);
+    });
+
+  // Here's a list of repos!
+});
