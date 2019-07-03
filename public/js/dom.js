155fe00bbbfaa6b26dc08d088de55c57034bbff6
wajdomahdi <eparis@atlassian.com>
2019-07-03 16:15:06 +0300

    refactor code

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,38 +1,51 @@
-var img = document.getElementById("image");
-document.getElementById("submit").addEventListener("click", function() {
-  var word = document.getElementById("word").value;
-  fetch(
-    "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190703T060408Z.9a2c739ed70efe69.b5f4e317e79bd07c2d4923694f9c82b15c2aea60&text=" +
-      word +
-      "&lang=ar-en"
-  )
+function request(url, cb) {
+  fetch(url)
     .then(response => {
-      // here we're turning the response into JSON.
       return response.json();
-      console.log(response.json);
     })
     .then(data => {
       console.log(data);
-      document.querySelector("p").innerText = data.text[0];
-      var meaning = data.text[0];
-      fetch(
-        "http://api.giphy.com/v1/gifs/search?q=" +
-          meaning +
-          "&api_key=p0FMwmf9fytJSZw4DQgJq6Vurt4S8oHv&limit=3"
-      )
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          console.log(data);
-          img.src = `https://media.giphy.com/media/${
-            data.data[2].id
-          }/giphy.gif`;
-        });
+      cb(data);
     })
     .catch(error => {
       console.log(error);
     });
+}
 
-  // Here's a list of repos!
+function translate(input, cb) {
+  request(
+    "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190703T060408Z.9a2c739ed70efe69.b5f4e317e79bd07c2d4923694f9c82b15c2aea60&text=" +
+      input +
+      "&lang=ar-en",
+    result => {
+      document.querySelector("p").innerText = result.text[0];
+      var meaning = result.text[0];
+      cb(meaning);
+    }
+  );
+}
+
+document.getElementById("submit").addEventListener("click", function() {
+  var word = document.getElementById("word").value;
+  translate(word, result => {
+    console.log("result", result);
+    request(
+      "http://api.giphy.com/v1/gifs/search?q=" +
+        result +
+        "&api_key=p0FMwmf9fytJSZw4DQgJq6Vurt4S8oHv&limit=4",
+      data => {
+        var div = document.querySelector(".container");
+        for (var i = 0; i < 4; i++) {
+          var img = document.createElement("img");
+          img.setAttribute("class", "image");
+
+          div.appendChild(img);
+
+          img.src = `https://media.giphy.com/media/${
+            data.data[i].id
+          }/giphy.gif`;
+        }
+      }
+    );
+  });
 });
