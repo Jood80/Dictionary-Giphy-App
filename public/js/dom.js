40e6004cdfe441a3c86b4d8722227c4b95378331
wajdomahdi <eparis@atlassian.com>
2019-07-03 13:54:05 +0300

    add images API Related #9

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,3 +1,4 @@
+var img = document.getElementById("image");
 document.getElementById("submit").addEventListener("click", function() {
   var word = document.getElementById("word").value;
   fetch(
@@ -13,6 +14,21 @@ document.getElementById("submit").addEventListener("click", function() {
     .then(data => {
       console.log(data);
       document.querySelector("p").innerText = data.text[0];
+      var meaning = data.text[0];
+      fetch(
+        "http://api.giphy.com/v1/gifs/search?q=" +
+          meaning +
+          "&api_key=p0FMwmf9fytJSZw4DQgJq6Vurt4S8oHv&limit=3"
+      )
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          img.src = `https://media.giphy.com/media/${
+            data.data[2].id
+          }/giphy.gif`;
+        });
     })
     .catch(error => {
       console.log(error);
